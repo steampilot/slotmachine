b41d2be418138318b424285bf2f039a072bf0ae7
Jerome Roethlisberger <jerome.roeth@gmail.com>
2014-09-07 15:28:13 +0200

    Finished the slot machine

---
+++ js/slotmachine.js
@@ -0,0 +1,48 @@
+/**
+ * Created by ShinKenDo on 07.09.14.
+ */
+var images = [
+	'slot1.png',
+	'slot2.png',
+	'slot3.png',
+	'slot4.png',
+	'slot5.png',
+	'slot6.png'
+];
+var slots = [];
+var score = 10;
+
+function startGame(){
+	document.getElementById('score').innerHTML=score+" - Rolling!";
+	var seconds = 1000; //miliseconds
+	shuffle();
+	document.getElementById('arm').src='img/arm-2.png';
+	setTimeout(function(){document.getElementById('arm').src='img/arm-1.png';},4*seconds);
+	setTimeout(function(){rollDice(1)},4*seconds);
+	setTimeout(function(){rollDice(2)},5*seconds);
+	setTimeout(function(){rollDice(3)},6*seconds);
+	setTimeout(function(){checkWinCondition()},6.5*seconds);
+}
+function shuffle(){
+	document.getElementById('window1').src='img/slot_rot1.gif';
+	document.getElementById('window2').src='img/slot_rot2.gif';
+	document.getElementById('window3').src='img/slot_rot3.gif';
+}
+function rollDice(window){
+		var rollDice = Math.round(Math.random() * 5);
+		document.getElementById('window'+window).src='img/'+images[rollDice];
+		slots[window] = rollDice;
+}
+function checkWinCondition(){
+	if((slots[1] === slots[2]) && (slots[2] === slots[3])){
+		alert("Jackpot" + score);
+	} else if(slots[1] === slots[2] || slots[2] === slots[3] || slots[3] == slots[1]) {
+		document.getElementById('score').innerHTML=score+" - Try again for free!";
+	} else {
+		score --;
+		if (score < 1) {alert("Game Over");}
+		else{
+			document.getElementById('score').innerHTML=score+" - Try again! Insert Coin!";
+		}
+	}
+}
